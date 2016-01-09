826b54ea23558d11b7733efda6cd04799a94e2e5
Anton Domashnev <antondomashnev@gmail.com>
2016-01-09 23:35:39 +0100

    Initial Commit

---
+++ PuzzleAnimation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PuzzleAnimation
+//
+//  Created by Anton Domashnev on 1/9/16.
+//  Copyright © 2016 Anton Domashnev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
